8bca1c78275bbc6f4525c12371eb1490c4549466
RaulORios <raul.orios89@outlook.com>
2021-10-14 13:12:30 -0300

    Arquivos atualizados

---
+++ src/Heranca_Orient_Objeto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Heranca_Orient_Objeto.Entitties;
+
+namespace Heranca_Orient_Objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(0010, "Bob Brown", 100.00, 500.00);
+
+            Console.WriteLine(account.Balance);
+
+            Console.ReadKey();
+
+        }
+    }
+}
